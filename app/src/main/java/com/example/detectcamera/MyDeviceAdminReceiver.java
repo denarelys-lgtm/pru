3ea2f1fbeaaa4d4c00370dca5d179daad92f20f5
denarelys-lgtm <denarelys@gmail.com>
2026-08-10 08:40:09 -0400

    Create MyDeviceAdminReceiver.java

---
+++ app/src/main/java/com/example/detectcamera/MyDeviceAdminReceiver.java
@@ -0,0 +1,14 @@
+package com.example.detectcamera;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
+        Toast.makeText(context, "Modo Device Owner Activado", Toast.LENGTH_SHORT).show();
+    }
+}
