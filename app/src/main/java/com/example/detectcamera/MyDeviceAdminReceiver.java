f3a30d8bd185085d933ae47742ed2d1227a53f55
denarelys-lgtm <denarelys@gmail.com>
2026-08-11 06:31:05 -0400

    Update MyDeviceAdminReceiver.java

--- app/src/main/java/com/example/detectcamera/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/example/detectcamera/MyDeviceAdminReceiver.java
@@ -9,6 +9,6 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
     @Override
     public void onEnabled(Context context, Intent intent) {
         super.onEnabled(context, intent);
-        Toast.makeText(context, "Modo Device Owner Activado con éxito", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Modo Device Owner Activado", Toast.LENGTH_SHORT).show();
     }
 }
